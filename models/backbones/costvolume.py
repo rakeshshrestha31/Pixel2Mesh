842c6ec2ba098a878be084acd666e89f1dcd0680
waynefan.fzw <waynefan.fzw@alibaba-inc.com>
2020-01-08 17:10:13 +0800

    costvolume: change vgg network from tensorflow aligned to vggp2m

--- models/backbones/costvolume.py
+++ models/backbones/costvolume.py
@@ -152,8 +152,8 @@ class MVSNet(nn.Module):
     def __init__(self):
         super(MVSNet, self).__init__()
 
-        self.feature = VGG16TensorflowAlign()
         self.features_dim = 960 + 191
+        self.feature = VGG16P2M()
         # self.features_dim = 384
         self.cost_regularization = CostRegNet()
 
@@ -217,70 +217,85 @@ class MVSNet(nn.Module):
         features[0].append(cost_agg_feature)
         return {"features": features, "depth":depth}
 
+class VGG16P2M(nn.Module):
 
-class VGG16TensorflowAlign(nn.Module):
+    def __init__(self, n_classes_input=3, pretrained=False):
+        super(VGG16P2M, self).__init__()
 
-    def __init__(self, n_classes_input=3):
-        super(VGG16TensorflowAlign, self).__init__()
+        self.features_dim = 960
 
-        self.features_dim = 960 #191
-        # this is to align with tensorflow padding (with stride)
-        # https://bugxch.github.io/tf%E4%B8%AD%E7%9A%84padding%E6%96%B9%E5%BC%8FSAME%E5%92%8CVALID%E6%9C%89%E4%BB%80%E4%B9%88%E5%8C%BA%E5%88%AB/
-        self.same_padding = nn.ZeroPad2d(1)
-        self.tf_padding = nn.ZeroPad2d((0, 1, 0, 1))
-        self.tf_padding_2 = nn.ZeroPad2d((1, 2, 1, 2))
+        self.conv0_1 = nn.Conv2d(n_classes_input, 16, 3, stride=1, padding=1)
+        self.conv0_2 = nn.Conv2d(16, 16, 3, stride=1, padding=1)
 
-        self.conv0_1 = nn.Conv2d(n_classes_input, 16, 3, stride=1, padding=0)
-        self.conv0_2 = nn.Conv2d(16, 16, 3, stride=1, padding=0)
+        self.conv1_1 = nn.Conv2d(16, 32, 3, stride=2, padding=1)  # 224 -> 112
+        self.conv1_2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+        self.conv1_3 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
 
-        self.conv1_1 = nn.Conv2d(16, 32, 3, stride=2, padding=0)  # 224 -> 112
-        self.conv1_2 = nn.Conv2d(32, 32, 3, stride=1, padding=0)
-        self.conv1_3 = nn.Conv2d(32, 32, 3, stride=1, padding=0)
+        self.conv2_1 = nn.Conv2d(32, 64, 3, stride=2, padding=1)  # 112 -> 56
+        self.conv2_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.conv2_3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
 
-        self.conv2_1 = nn.Conv2d(32, 64, 3, stride=2, padding=0)  # 112 -> 56
-        self.conv2_2 = nn.Conv2d(64, 64, 3, stride=1, padding=0)
-        self.conv2_3 = nn.Conv2d(64, 64, 3, stride=1, padding=0)
+        self.conv3_1 = nn.Conv2d(64, 128, 3, stride=2, padding=1)  # 56 -> 28
+        self.conv3_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.conv3_3 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
 
-        self.conv3_1 = nn.Conv2d(64, 128, 3, stride=2, padding=0)  # 56 -> 28
-        self.conv3_2 = nn.Conv2d(128, 128, 3, stride=1, padding=0)
-        self.conv3_3 = nn.Conv2d(128, 128, 3, stride=1, padding=0)
+        self.conv4_1 = nn.Conv2d(128, 256, 5, stride=2, padding=2)  # 28 -> 14
+        self.conv4_2 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv4_3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
 
-        self.conv4_1 = nn.Conv2d(128, 256, 5, stride=2, padding=0)  # 28 -> 14
-        self.conv4_2 = nn.Conv2d(256, 256, 3, stride=1, padding=0)
-        self.conv4_3 = nn.Conv2d(256, 256, 3, stride=1, padding=0)
+        self.conv5_1 = nn.Conv2d(256, 512, 5, stride=2, padding=2)  # 14 -> 7
+        self.conv5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv5_4 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
 
-        self.conv5_1 = nn.Conv2d(256, 512, 5, stride=2, padding=0)  # 14 -> 7
-        self.conv5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=0)
-        self.conv5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=0)
-        self.conv5_4 = nn.Conv2d(512, 512, 3, stride=1, padding=0)
+        if "vgg16p2m" in config.PRETRAINED_WEIGHTS_PATH and pretrained:
+            state_dict = torch.load(config.PRETRAINED_WEIGHTS_PATH["vgg16p2m"])
+            self.load_state_dict(state_dict)
+        else:
+            self._initialize_weights()  # not load the pre-trained model
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, img):
-        img = F.relu(self.conv0_1(self.same_padding(img)))
-        img = F.relu(self.conv0_2(self.same_padding(img)))
+        img = F.relu(self.conv0_1(img))
+        img = F.relu(self.conv0_2(img))
+        # img0 = torch.squeeze(img) # 224
 
-        img = F.relu(self.conv1_1(self.tf_padding(img)))
-        img = F.relu(self.conv1_2(self.same_padding(img)))
-        img = F.relu(self.conv1_3(self.same_padding(img)))
+        img = F.relu(self.conv1_1(img))
+        img = F.relu(self.conv1_2(img))
+        img = F.relu(self.conv1_3(img))
+        # img1 = torch.squeeze(img) # 112
 
-        img = F.relu(self.conv2_1(self.tf_padding(img)))
-        img = F.relu(self.conv2_2(self.same_padding(img)))
-        img = F.relu(self.conv2_3(self.same_padding(img)))
+        img = F.relu(self.conv2_1(img))
+        img = F.relu(self.conv2_2(img))
+        img = F.relu(self.conv2_3(img))
         img2 = img
 
-        img = F.relu(self.conv3_1(self.tf_padding(img)))
-        img = F.relu(self.conv3_2(self.same_padding(img)))
-        img = F.relu(self.conv3_3(self.same_padding(img)))
+        img = F.relu(self.conv3_1(img))
+        img = F.relu(self.conv3_2(img))
+        img = F.relu(self.conv3_3(img))
         img3 = img
 
-        img = F.relu(self.conv4_1(self.tf_padding_2(img)))
-        img = F.relu(self.conv4_2(self.same_padding(img)))
-        img = F.relu(self.conv4_3(self.same_padding(img)))
+        img = F.relu(self.conv4_1(img))
+        img = F.relu(self.conv4_2(img))
+        img = F.relu(self.conv4_3(img))
         img4 = img
 
-        img = F.relu(self.conv5_1(self.tf_padding_2(img)))
-        img = F.relu(self.conv5_2(self.same_padding(img)))
-        img = F.relu(self.conv5_3(self.same_padding(img)))
-        img = F.relu(self.conv5_4(self.same_padding(img)))
+        img = F.relu(self.conv5_1(img))
+        img = F.relu(self.conv5_2(img))
+        img = F.relu(self.conv5_3(img))
+        img = F.relu(self.conv5_4(img))
         img5 = img
 
         return [img2, img3, img4, img5]
